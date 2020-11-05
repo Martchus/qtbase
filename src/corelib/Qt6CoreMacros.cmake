4bc7d8f91f0f3695ff167bf9617510942097c82a
Martchus <martchus@gmx.net>
2020-11-05 22:08:10 +0100

    Use CMake's default import library suffix
    Change-Id: I3dbedaec74683e5bfd008f7f2fd1e046dfb921b2

--- src/corelib/Qt6CoreMacros.cmake
+++ src/corelib/Qt6CoreMacros.cmake
@@ -447,7 +447,6 @@ function(_qt_internal_apply_win_prefix_and_suffix target)
 
         # This should cover both MINGW with GCC and CLANG.
         if(NOT MSVC)
-            set_property(TARGET "${target}" PROPERTY IMPORT_SUFFIX ".a")
 
             get_target_property(target_type ${target} TYPE)
             if(target_type STREQUAL "STATIC_LIBRARY")
