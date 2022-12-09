0c5d2099d1bef74a161898537fbbe9203da5a4ff
Eirik Aavitsland <eirik.aavitsland@qt.io>
2022-12-09 10:46:44 +0100

    Doc: note QWindow::winId() will cause platform window creation
    Worth it to emphasize this, as such a side effect from a const
    accessor may well be unexpected.
    
    Change-Id: I0b70d81927e0b66799cbe736ce5d488e2ec77ce0
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    Reviewed-by: Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
    Reviewed-by: Mikołaj Boc <Mikolaj.Boc@qt.io>
    (cherry picked from commit b1bb732639be849b225db635a725b3c19881380c)

--- src/gui/kernel/qwindow.cpp
+++ src/gui/kernel/qwindow.cpp
@@ -644,7 +644,7 @@ bool QWindow::isVisible() const
     into an actual native surface. However, the window remains hidden until setVisible() is called.
 
     Note that it is not usually necessary to call this function directly, as it will be implicitly
-    called by show(), setVisible(), and other functions that require access to the platform
+    called by show(), setVisible(), winId(), and other functions that require access to the platform
     resources.
 
     Call destroy() to free the platform resources if necessary.
