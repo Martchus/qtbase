e3b54a890e32fcc449b2456003fe1fb912332fd6
David Faure <david.faure@kdab.com>
2023-08-28 17:42:02 +0200

    QMimeDatabase: fix detection of pattern conflict in different prefixes
    Installing a second mimetype with *.txt as glob had a different
    effect depending on whether it was installed into the same prefix
    or a different prefix as the one where text/plain is installed.
    
    Pick-to: 6.6
    Change-Id: I7f54b8efe22f620eb57257745c48fe5402c87626
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    (based on commit 1b39e61a775d70ee96287e9f0e418cb5741e6638)

--- tests/auto/corelib/mimetypes/qmimedatabase/tst_qmimedatabase.cpp
+++ tests/auto/corelib/mimetypes/qmimedatabase/tst_qmimedatabase.cpp
@@ -49,6 +49,7 @@ static const char *const additionalMimeFiles[] = {
     "yast2-metapackage-handler-mimetypes.xml",
     "qml-again.xml",
     "text-x-objcsrc.xml",
+    "text-plain-subclass.xml",
     "invalid-magic1.xml",
     "invalid-magic2.xml",
     "invalid-magic3.xml",
@@ -1165,6 +1166,8 @@ void tst_QMimeDatabase::installNewLocalMimeType()
         QVERIFY(objcsrc.isValid());
         QCOMPARE(objcsrc.globPatterns(), QStringList());
     }
+    QCOMPARE(db.mimeTypeForFile(QLatin1String("foo.txt"), QMimeDatabase::MatchExtension).name(),
+             QString::fromLatin1("text/plain"));
 
     // Test that a double-definition of a mimetype doesn't lead to sniffing ("conflicting globs").
     const QString qmlTestFile = s_additionalFilesResourcePrefix + QLatin1String("test.qml");
